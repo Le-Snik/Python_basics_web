bf8d4211af1496832fe4fd8bc0d0495bd0360007
Le-Snik <65533557+Le-Snik@users.noreply.github.com>
2021-03-15 21:18:05 +0300

    Сделаны 1 и 2 ( 3) задания

---
+++ Nikolskiy_Aleksey_dz_2/task2_2.py
@@ -0,0 +1,26 @@
+my_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+string = ''
+signs = ['-', '+']
+
+i = 0
+while i != len(my_list):
+    if my_list[i].isdigit() or ((my_list[i][0] in signs) and my_list[i][1:].isdigit):  # Если элемент целиком из цифр,
+        # или первый символ элемента +- а в остальном он из цифр
+        if my_list[i].isdigit() and int(my_list[i]) < 10:  # Добавялем 0 , если не двузначный
+            my_list[i] = "0" + my_list[i]
+        elif (my_list[i][0] in signs) and my_list[i][1:].isdigit and int(my_list[i][1:]) < 10: #для элементов со знаком
+            my_list[i] = my_list[i][0] + "0" + my_list[i][1:]
+
+        my_list.insert(i, '"')
+        my_list.insert(i + 2, '"')
+        i += 1
+    i += 1
+
+for i in my_list:
+    if i.isalpha() and len(string) != 0:
+        string = string + ' ' + i + ' '
+    elif len(string) == 0:
+        string = string + i + ' '
+    else:
+        string = string + i
+print(string)
