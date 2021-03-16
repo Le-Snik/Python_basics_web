55094672291cc7e13b9567dd0af07e70917a6092
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2021-03-16 09:02:50 +0300

    1

--- Nikolskiy_Aleksey_dz_2/task2_4.py
+++
@@ -1,6 +0,0 @@
-my_list = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
-for i in range(len(my_list)):
-    my_list[i] = my_list[i].title()
-    my_list[i] = my_list[i].split(' ')
-    print("Привет,", my_list[i][-1])
-
