7be16cfe57f57ba8946a3c68a3be71f623ab2102
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2021-03-25 14:59:19 +0300

    Update task5_5.py

--- Nikolskiy_Aleksey_dz_5/task5_5.py
+++ Nikolskiy_Aleksey_dz_5/task5_5.py
@@ -1,12 +1,16 @@
-src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+src = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 2, 10, 7, 4, 11]
 unique_src = set()
+buff_set = set()
 
 # получаем уникальное множество
 for i in src:
-    if i in unique_src:
+    if i in unique_src and i in buff_set:
         unique_src.remove(i)
-    else:
+    elif not (i in buff_set):
         unique_src.add(i)
+    buff_set.add(i)
+
+
 print(unique_src)
 
 # Если элемент есть в уникальном множесте, т опереписываем его  в новый список, для сохранения порядкового номера
