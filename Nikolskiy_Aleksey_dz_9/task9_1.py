0b9a99d286375743b612e8e7318d28d691294695
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2021-04-09 13:48:47 +0300

    Переделаны 1 и 3 , сделано 4ое

--- Nikolskiy_Aleksey_dz_9/task9_1.py
+++ Nikolskiy_Aleksey_dz_9/task9_1.py
@@ -1,25 +1,39 @@
 import time
+import colorama
+import sys
+from colorama import Fore
+
+colorama.init()
+
+
+def time_out(color, times):
+    for _ in reversed(range(0, times)):
+        print(f"{color} {_}", end="")
+        time.sleep(1)
+        print(' '"\r", end="")
 
 
 class TrafficLight:
     def __init__(self, color='red'):
         self.__color = color
-        #self.running()
+        self.running()
 
     def running(self):
         if self.__color == 'red':
-            print(self.__color)
-            time.sleep(7)
+            time_out(Fore.RED + self.__color, 7)
+            self.__color = 'yellow'
+
         if self.__color == 'yellow':
-            print(self.__color)
-            time.sleep(2)
+            time_out(Fore.YELLOW + self.__color, 2)
             self.__color = 'green'
+
         if self.__color == 'green':
-            print(self.__color)
-            time.sleep(10)
+            time_out(Fore.CYAN + self.__color, 10)
             self.__color = 'red'
 
 
 light1 = TrafficLight()
-light1.running()
 
+loops = int(sys.argv[1])
+for i in range(0, loops):
+    light1.running()
