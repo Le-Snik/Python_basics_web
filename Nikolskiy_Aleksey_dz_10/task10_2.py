5055fe399f91b0e191b0e29bfd2000bfa1a023a4
Aleksey Nikolskii <aleksey.nikolskiy4@rsce.ru>
2021-04-13 15:15:35 +0300

    Create task10_2.py

---
+++ Nikolskiy_Aleksey_dz_10/task10_2.py
@@ -0,0 +1,31 @@
+class Clothes:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def get_cons(self):
+        self.cons = round(self._multi * self._modifier + 0.5, 2)
+        return self.cons
+
+
+class Coat(Clothes):
+    def __init__(self, name, size):
+        self._modifier = 1 / 6.5
+        self.size = size
+        self._multi = self.size
+        super().__init__(name)
+
+
+class Suit(Clothes):
+    def __init__(self, name, height):
+        self._modifier = 2
+        self.height = height
+        self._multi = self.height
+        super().__init__(name)
+
+
+suit = Suit("best_suit", 5)
+print(suit.get_cons)
+
+coat = Coat("best_coat", 6)
+print(coat.get_cons)
