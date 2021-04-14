bb9ed557e0f6f34c065ab1c8211e97ae31a9efca
Le-Snik <65533557+Le-Snik@users.noreply.github.com>
2021-04-14 21:14:59 +0300

    1-2

--- Nikolskiy_Aleksey_dz_11/task11_1.py
+++ Nikolskiy_Aleksey_dz_11/task11_1.py
@@ -1,26 +1,41 @@
+import sys
+import datetime
+
+
 class NewDate:
     def __init__(self, str_date):
         NewDate.str_date = str(str_date)
 
     @classmethod
     def get_date(cls):
-        lst = cls.str_date.split('-')
+
         try:
+            lst = cls.str_date.split('-')
+
             cls.day = int(lst[0])
             cls.month = int(lst[1])
             cls.year = int(lst[2])
+            cls._valid = True
         except IndexError:
             print("неправильный формат даты")
+            cls._valid = False
+        except (AttributeError, ValueError):
+            print("Дата должна состоять только из цифр")
+            cls._valid = False
 
     @staticmethod
     def valid_date():
-        if not (1 < NewDate.day < 31) or not (1 < NewDate.month < 31):
-            print("invalid date")
+        if NewDate._valid:
+            try:
+                NewDate.as_date = datetime.date(NewDate.year, NewDate.month, NewDate.day)
+            except ValueError as err:
+                NewDate._valid = False
+                print(err)
 
 
-my_date = NewDate("12-68-1992")
+my_date = NewDate(str(sys.argv[1]))
 
 NewDate.get_date()
 NewDate.valid_date()
-
-print(f"{NewDate.day} {NewDate.month} {NewDate.year}")
+if NewDate._valid:
+    print(f"{NewDate.as_date.day} {NewDate.as_date.month} {NewDate.as_date.year}")
